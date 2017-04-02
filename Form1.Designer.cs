4543636b20f740a75b240571379094e6ad572bee
Nathalia Adriele <nat-adriele@hotmail.com>
2017-04-02 20:49:51 -0300

    Projeto Jogo Dado

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace JogoDado
+{
+    partial class frmJogoDoDado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param> 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblJogada01 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtJogada1Jogador1 = new System.Windows.Forms.TextBox();
+            this.txtJogada2Jogador1 = new System.Windows.Forms.TextBox();
+            this.txtJogada1Jogador2 = new System.Windows.Forms.TextBox();
+            this.txtJogada2Jogador2 = new System.Windows.Forms.TextBox();
+            this.txtJogada3Jogador1 = new System.Windows.Forms.TextBox();
+            this.txtJogada3Jogador2 = new System.Windows.Forms.TextBox();
+            this.lblJogada02 = new System.Windows.Forms.Label();
+            this.lblJogada03 = new System.Windows.Forms.Label();
+            this.btnJogador01 = new System.Windows.Forms.Button();
+            this.btnJogador2 = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.X = new System.Windows.Forms.Label();
+            this.imgDado = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgDado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblJogada01
+            // 
+            this.lblJogada01.AutoSize = true;
+            this.lblJogada01.BackColor = System.Drawing.Color.White;
+            this.lblJogada01.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogada01.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblJogada01.Location = new System.Drawing.Point(105, 79);
+            this.lblJogada01.Name = "lblJogada01";
+            this.lblJogada01.Size = new System.Drawing.Size(128, 20);
+            this.lblJogada01.TabIndex = 1;
+            this.lblJogada01.Text = "Primeira Jogada";
+            this.lblJogada01.Click += new System.EventHandler(this.lblJogada1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(208, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jogador 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(352, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jogador 2";
+            // 
+            // txtJogada1Jogador1
+            // 
+            this.txtJogada1Jogador1.BackColor = System.Drawing.Color.White;
+            this.txtJogada1Jogador1.Enabled = false;
+            this.txtJogada1Jogador1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtJogada1Jogador1.Location = new System.Drawing.Point(246, 81);
+            this.txtJogada1Jogador1.Name = "txtJogada1Jogador1";
+            this.txtJogada1Jogador1.Size = new System.Drawing.Size(42, 20);
+            this.txtJogada1Jogador1.TabIndex = 4;
+            this.txtJogada1Jogador1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtJogada1Jogador1.TextChanged += new System.EventHandler(this.txtbLance1Jogador1_TextChanged);
+            // 
+            // txtJogada2Jogador1
+            // 
+            this.txtJogada2Jogador1.BackColor = System.Drawing.Color.White;
+            this.txtJogada2Jogador1.Enabled = false;
+            this.txtJogada2Jogador1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtJogada2Jogador1.Location = new System.Drawing.Point(246, 122);
+            this.txtJogada2Jogador1.Name = "txtJogada2Jogador1";
+            this.txtJogada2Jogador1.Size = new System.Drawing.Size(42, 20);
+            this.txtJogada2Jogador1.TabIndex = 5;
+            this.txtJogada2Jogador1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtJogada1Jogador2
+            // 
+            this.txtJogada1Jogador2.BackColor = System.Drawing.Color.White;
+            this.txtJogada1Jogador2.Enabled = false;
+            this.txtJogada1Jogador2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtJogada1Jogador2.Location = new System.Drawing.Point(366, 81);
+            this.txtJogada1Jogador2.Name = "txtJogada1Jogador2";
+            this.txtJogada1Jogador2.Size = new System.Drawing.Size(42, 20);
+            this.txtJogada1Jogador2.TabIndex = 6;
+            this.txtJogada1Jogador2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtJogada2Jogador2
+            // 
+            this.txtJogada2Jogador2.BackColor = System.Drawing.Color.White;
+            this.txtJogada2Jogador2.Enabled = false;
+            this.txtJogada2Jogador2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtJogada2Jogador2.Location = new System.Drawing.Point(366, 122);
+            this.txtJogada2Jogador2.Name = "txtJogada2Jogador2";
+            this.txtJogada2Jogador2.Size = new System.Drawing.Size(42, 20);
+            this.txtJogada2Jogador2.TabIndex = 7;
+            this.txtJogada2Jogador2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtJogada3Jogador1
+            // 
+            this.txtJogada3Jogador1.BackColor = System.Drawing.Color.White;
+            this.txtJogada3Jogador1.Enabled = false;
+            this.txtJogada3Jogador1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtJogada3Jogador1.Location = new System.Drawing.Point(246, 164);
+            this.txtJogada3Jogador1.Name = "txtJogada3Jogador1";
+            this.txtJogada3Jogador1.Size = new System.Drawing.Size(42, 20);
+            this.txtJogada3Jogador1.TabIndex = 8;
+            this.txtJogada3Jogador1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtJogada3Jogador2
+            // 
+            this.txtJogada3Jogador2.BackColor = System.Drawing.Color.White;
+            this.txtJogada3Jogador2.Enabled = false;
+            this.txtJogada3Jogador2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtJogada3Jogador2.Location = new System.Drawing.Point(366, 164);
+            this.txtJogada3Jogador2.Name = "txtJogada3Jogador2";
+            this.txtJogada3Jogador2.Size = new System.Drawing.Size(42, 20);
+            this.txtJogada3Jogador2.TabIndex = 9;
+            this.txtJogada3Jogador2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblJogada02
+            // 
+            this.lblJogada02.AutoSize = true;
+            this.lblJogada02.BackColor = System.Drawing.Color.White;
+            this.lblJogada02.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogada02.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblJogada02.Location = new System.Drawing.Point(105, 122);
+            this.lblJogada02.Name = "lblJogada02";
+            this.lblJogada02.Size = new System.Drawing.Size(135, 20);
+            this.lblJogada02.TabIndex = 1;
+            this.lblJogada02.Text = "Segunda Jogada";
+            // 
+            // lblJogada03
+            // 
+            this.lblJogada03.AutoSize = true;
+            this.lblJogada03.BackColor = System.Drawing.Color.White;
+            this.lblJogada03.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogada03.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblJogada03.Location = new System.Drawing.Point(118, 162);
+            this.lblJogada03.Name = "lblJogada03";
+            this.lblJogada03.Size = new System.Drawing.Size(115, 20);
+            this.lblJogada03.TabIndex = 1;
+            this.lblJogada03.Text = "Ultima Jogada";
+            // 
+            // btnJogador01
+            // 
+            this.btnJogador01.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnJogador01.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogador01.ForeColor = System.Drawing.Color.Black;
+            this.btnJogador01.Location = new System.Drawing.Point(212, 222);
+            this.btnJogador01.Name = "btnJogador01";
+            this.btnJogador01.Size = new System.Drawing.Size(96, 23);
+            this.btnJogador01.TabIndex = 10;
+            this.btnJogador01.Tag = "1";
+            this.btnJogador01.Text = "Jogador 1";
+            this.btnJogador01.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnJogador01.UseVisualStyleBackColor = false;
+            this.btnJogador01.Click += new System.EventHandler(this.btnJogador1_Click);
+            // 
+            // btnJogador2
+            // 
+            this.btnJogador2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnJogador2.Enabled = false;
+            this.btnJogador2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogador2.Location = new System.Drawing.Point(339, 223);
+            this.btnJogador2.Name = "btnJogador2";
+            this.btnJogador2.Size = new System.Drawing.Size(96, 23);
+            this.btnJogador2.TabIndex = 11;
+            this.btnJogador2.Tag = "1";
+            this.btnJogador2.Text = "Jogador 2";
+            this.btnJogador2.UseVisualStyleBackColor = false;
+            this.btnJogador2.Click += new System.EventHandler(this.btnJogador2_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 200;
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.BackColor = System.Drawing.Color.White;
+            this.X.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.X.Location = new System.Drawing.Point(315, 42);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(14, 13);
+            this.X.TabIndex = 12;
+            this.X.Text = "X";
+            // 
+            // imgDado
+            // 
+            this.imgDado.Image = global::JogoDado.Properties.Resources.dado;
+            this.imgDado.Location = new System.Drawing.Point(12, 12);
+            this.imgDado.Name = "imgDado";
+            this.imgDado.Size = new System.Drawing.Size(81, 64);
+            this.imgDado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgDado.TabIndex = 0;
+            this.imgDado.TabStop = false;
+            // 
+            // frmJogoDoDado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(553, 268);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.btnJogador2);
+            this.Controls.Add(this.btnJogador01);
+            this.Controls.Add(this.txtJogada3Jogador2);
+            this.Controls.Add(this.txtJogada3Jogador1);
+            this.Controls.Add(this.txtJogada2Jogador2);
+            this.Controls.Add(this.txtJogada1Jogador2);
+            this.Controls.Add(this.txtJogada2Jogador1);
+            this.Controls.Add(this.txtJogada1Jogador1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblJogada03);
+            this.Controls.Add(this.lblJogada02);
+            this.Controls.Add(this.lblJogada01);
+            this.Controls.Add(this.imgDado);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "frmJogoDoDado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo do Dado";
+            this.Load += new System.EventHandler(this.frmJogoDado_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgDado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblJogada01;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtJogada1Jogador1;
+        private System.Windows.Forms.TextBox txtJogada2Jogador1;
+        private System.Windows.Forms.TextBox txtJogada1Jogador2;
+        private System.Windows.Forms.TextBox txtJogada2Jogador2;
+        private System.Windows.Forms.TextBox txtJogada3Jogador1;
+        private System.Windows.Forms.TextBox txtJogada3Jogador2;
+        private System.Windows.Forms.Label lblJogada02;
+        private System.Windows.Forms.Label lblJogada03;
+        private System.Windows.Forms.Button btnJogador01;
+        private System.Windows.Forms.Button btnJogador2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label X;
+        private System.Windows.Forms.PictureBox imgDado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDado
+{
+    public partial class frmJogoDoDado : Form
+    {
+        private int jogador1;
+        private int jogador2;
+
+        public frmJogoDoDado() 
+        {
+            InitializeComponent();
+        }
+        private void btnJogador1_Click(object sender, EventArgs e)
+        {
+            String jogada = btnJogador01.Tag.ToString(); //bnt do jogador 1, gerar numero do dado aleatório
+            Random rnd = new Random();
+            int valorDado = rnd.Next(1, 7);
+            selecionarODado(valorDado);
+            switch (jogada)
+            {
+                case "1": 
+                    txtJogada1Jogador1.Text = valorDado.ToString();
+                    btnJogador01.Tag = "2";
+                    btnJogador01.Enabled = false;
+                    btnJogador2.Enabled = true;
+
+                    break;
+
+                case "2": 
+                    txtJogada2Jogador1.Text = valorDado.ToString();
+                    btnJogador01.Tag = "3";
+                    btnJogador01.Enabled = false;
+                    btnJogador2.Enabled = true;
+
+                    break;
+
+                case "3": 
+                    txtJogada3Jogador1.Text = valorDado.ToString();
+                    btnJogador01.Enabled = false;
+                    btnJogador2.Enabled = true;
+                    break;
+            }      
+        }
+        private void btnJogador2_Click(object sender, EventArgs e)
+        {
+            String jogada = btnJogador2.Tag.ToString(); //botao do jogador 2, gerar numero do dado aleatório
+            Random rnd = new Random();
+            int valorDado = rnd.Next(1, 7);
+            selecionarODado(valorDado);
+            switch (jogada)
+            {
+                case "1":
+                    txtJogada1Jogador2.Text = valorDado.ToString();
+                    btnJogador2.Tag = "2";
+                    btnJogador2.Enabled = false;
+                    btnJogador01.Enabled = true;
+
+                    break;
+
+                case "2":
+                    txtJogada2Jogador2.Text = valorDado.ToString();
+                    btnJogador2.Tag = "3";
+                    btnJogador2.Enabled = false;
+                    btnJogador01.Enabled = true;
+                    verificaQuemGanhou(jogada);
+
+                    break;
+
+                case "3":
+                    txtJogada3Jogador2.Text = valorDado.ToString();
+                    btnJogador2.Enabled = false;
+                    verificaQuemGanhou(jogada);
+
+                    break;
+            }
+        }
+        private void verificaQuemGanhou(string jogada)
+        {
+            switch (jogada) //jogada 1
+                //se o valor do jogador 1 for maior que do jogador 2 entao jogador 1 +=1 senao se for menor jogador 2 +=1;
+                            
+            {
+                case "2":
+                    if (Int32.Parse(txtJogada1Jogador1.Text) > Int32.Parse(txtJogada1Jogador2.Text))
+                    {
+                        jogador1 += 1;
+                    }
+                    else if (Int32.Parse(txtJogada1Jogador1.Text) < Int32.Parse(txtJogada1Jogador2.Text))
+                    {
+                        jogador2 += 1;
+                    }
+                    //jogada 2
+                    if (Int32.Parse(txtJogada2Jogador1.Text) > Int32.Parse(txtJogada2Jogador2.Text)) 
+                    {
+                        jogador1 += 1;
+                    }
+                    else if (Int32.Parse(txtJogada2Jogador1.Text) < Int32.Parse(txtJogada2Jogador2.Text))
+                    {
+                        jogador2 += 1;
+                    }
+                    if (jogador1 > jogador2 && jogador1 > 1) //se um jogador vencer as duas primeira- vencedor.
+                    {
+                        MessageBox.Show("JOGADOR 1 É O VENCEDOR!");
+                        Application.Exit();
+                    }
+                    else if (jogador2 > jogador1 && jogador2 > 1) //senao jogador 2 obtiver numero maior que jogador 1 e jogador 2 maior que 1- venceu.
+                    {
+                        MessageBox.Show("JOGADOR 2 É O VENCEDOR!");
+                        Application.Exit();
+                    }
+                    break;
+
+                case "3": //se um jogador ganhar a primeira partida e outro ganhar a segunda partida então ir para a ultima partida.
+                    if (Int32.Parse(txtJogada3Jogador1.Text) > Int32.Parse(txtJogada3Jogador2.Text))
+                    {
+                            jogador1 += 1;
+                    }
+                    else if (Int32.Parse(txtJogada3Jogador1.Text) < Int32.Parse(txtJogada3Jogador2.Text))
+                    {
+                             jogador2 += 1;
+                    }
+                    if (jogador1 > jogador2)
+                    {
+                            MessageBox.Show("PARABENS JOGADOR 1 VENCEDOR!");
+                            Application.Exit();
+                    }
+                    else if (jogador2 > jogador1)
+                    {
+                            MessageBox.Show("PARABENS JOGADOR 2 VENCEDOR!");
+                            Application.Exit();
+                    }
+                    else if (jogador2 == jogador1) //se na jogada 1 2 e 3 os valores forem os mesmos, então mostrar empate.
+                    {
+                            MessageBox.Show("Jogador 1 e Jogador 2 EMPATE!");
+                            Application.Exit();
+                    }
+                    break;
+            }
+        }
+        private void selecionarODado(int valorDado)
+        {
+            switch (valorDado) //aqui deve ser mostrado cada img com o valor do dado gerado em cada jogada
+            {
+                case 1: imgDado.Image = JogoDado.Properties.Resources._1;
+                    break;
+                case 2: imgDado.Image = JogoDado.Properties.Resources._2;
+                    break;
+                case 3: imgDado.Image = JogoDado.Properties.Resources._3;
+                    break;
+                case 4: imgDado.Image = JogoDado.Properties.Resources._4;
+                    break;
+                case 5: imgDado.Image = JogoDado.Properties.Resources._5;
+                    break;
+                case 6: imgDado.Image = JogoDado.Properties.Resources._6;
+                    break;
+            }
+        }
+        private void frmJogoDado_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void txtbLance1Jogador1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void lblJogada1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgDado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
